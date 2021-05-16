3f4eb0efc17fdcf8da5a777dcd36ac00ce9ceecc
shikechen <806335962@qq.com>
2021-05-16 23:22:50 +0800

    Update menu
    Up

--- app/src/main/java/com/shikechen/purekotlinandroid/chapter3/FirstActivity.kt
+++ app/src/main/java/com/shikechen/purekotlinandroid/chapter3/FirstActivity.kt
@@ -2,6 +2,8 @@ package com.shikechen.purekotlinandroid.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import com.shikechen.purekotlinandroid.R
@@ -17,5 +19,20 @@ class FirstActivity : AppCompatActivity() {
         button1.setOnClickListener {
             Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked add item",
+                    Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked remove item",
+                    Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
